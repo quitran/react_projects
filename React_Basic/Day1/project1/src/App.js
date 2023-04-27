1e3a0e6708874adde5821cafb9335ab4f59433bd
quitran <xitrumtigon@gmail.com>
2023-04-27 15:40:03 +0700

    Update code

--- React_Basic/Day1/project1/src/App.js
+++ React_Basic/Day1/project1/src/App.js
@@ -13,6 +13,11 @@ import EventBind from './components/EventBind';
 import ParentComponent from './components/ParentComponent';
 import UserGreeting from './components/UserGreeting';
 import NameList from './components/NameList';
+import Stylesheet from './components/Stylesheet';
+import Inline from './components/Inline';
+import './appStyles.css'
+import styles from './appStyles.module.css'
+import Form from './components/Form';
 
 function App() {
   return (
@@ -41,7 +46,12 @@ function App() {
       {/* <EventBind /> */}
       {/* <ParentComponent /> */}
       {/* <UserGreeting /> */}
-      <NameList />
+      {/* <NameList /> */}
+      {/* <Stylesheet primary={true}/>
+      <Inline />
+      <h1 className='error'> Error</h1>
+      <h1 className={styles.success}> Success</h1> */}
+      <Form />
     </div>
   );
 }
