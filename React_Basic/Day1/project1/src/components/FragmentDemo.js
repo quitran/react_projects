af8b206b1a5799e66dc1cd93d988bd175d29de7e
quitran <xitrumtigon@gmail.com>
2023-04-28 14:56:54 +0700

    Add files

---
+++ React_Basic/Day1/project1/src/components/FragmentDemo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class FragmentDemo extends Component {
+  render() {
+    return (
+      <div>FragmentDemo</div>
+    )
+  }
+}
+
+export default FragmentDemo
